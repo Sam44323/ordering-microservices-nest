1778341c87666075e62d071afb408e679c834c28
Suranjan <suranjanbiswas2002@gmail.com>
2022-11-23 00:25:19 +0530

    aadd: added support for validation-pipes

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
 bootstrap();
