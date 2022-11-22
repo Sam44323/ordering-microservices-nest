d6a543653791bc3ad9cbf50917265bfb75153d00
Suranjan <suranjanbiswas2002@gmail.com>
2022-11-22 23:05:04 +0530

    add: added default initiazlier for modules for  orders

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
@@ -7,6 +8,11 @@ import { OrdersService } from './orders.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      // using joi for schema-validation for env's
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [OrdersController],
