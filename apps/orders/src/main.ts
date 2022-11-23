23a6c9c272bceb64d6964f6a9981be80dbba364c
Suranjan <suranjanbiswas2002@gmail.com>
2022-11-23 12:56:28 +0530

    update: updated the configs

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -2,10 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
+import { RmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe()); // using a pipe for class-transformers
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice(rmqService.getOptions('BILLING'));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
--- libs/common/src/rmq/rmq.module.ts
+++ libs/common/src/rmq/rmq.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RmqService } from './rmq.service';
 
-@Module({})
+@Module({
+  exports: [RmqService],
+  providers: [RmqService],
+})
 export class RmqModule {}
